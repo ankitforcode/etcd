8699ba547551e7e24128639685f38549d3444bc4
Ankit Agarwal <ankit@Ankits-MacBook-Pro-2.local>
2017-12-13 10:47:48 +0530

    Enabling TLS for v3

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -18,6 +21,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/pkg/transport"
 )
 
 // Service :
@@ -58,46 +62,102 @@ type etcdMember struct {
 
 var (
 	localInstance         *ec2.Instance
-	etcdPeerPort          string
+	etcdMajorVersion      *int64
+	etcdPeerPort          *int64
 	peerProtocol          string
 	clientProtocol        string
-	etcdClientPort        string
-	etcdHeartbeatInterval uint
-	etcdElectionTimeout   uint
+	etcdClientPort        *int64
+	etcdCertFile          *string
+	etcdKeyFile           *string
+	etcdTrustedCaFile     *string
+	etcdPeerCertFile      *string
+	etcdPeerKeyFile       *string
+	etcdPeerTrustedCaFile *string
+	etcdHeartbeatInterval *int64
+	etcdElectionTimeout   *int64
+	clientTLSEnabled      bool
 )
 
 func main() {
 	svc := new(Service)
 	flag.StringVar(&svc.instance, "instance", "", "The instance ID of the cluster member. If not supplied, then the instance ID is determined from EC2 metadata")
 	flag.StringVar(&svc.clusterTagName, "clusterTagName", "aws:autoscaling:groupName", "The instance tag that is common to all members of the cluster")
-	defaultEtcdMajorVersion := "3"
-	if av := os.Getenv("ETCD_MAJOR_VERSION"); av != "" {
-		defaultEtcdMajorVersion = av
-	}
-	defaultLifecycleQueueName := ""
-	if lq := os.Getenv("LIFECYCLE_QUEUE_NAME"); lq != "" {
-		defaultLifecycleQueueName = lq
-	}
-	lifecycleQueueName := flag.String("lifecycle-queue-name", defaultLifecycleQueueName,
+
+	dataDir := flag.String("data-dir", "/var/lib/etcd",
+		"The path to the etcd data directory. "+
+			"Environment variable: ETCD_DATA_DIR")
+
+	lifecycleQueueName := flag.String("lifecycle-queue-name", "",
 		"The name of the lifecycle SQS queue (optional). "+
 			"Environment variable: LIFECYCLE_QUEUE_NAME")
-	defaultPeerPort := "2380"
-	if pp := os.Getenv("ETCD_PEER_PORT"); pp != "" {
-		defaultPeerPort = pp
-	}
-	flag.StringVar(&etcdPeerPort, "etcd-peer-port", defaultPeerPort,
-		"Etcd peer port number. "+
-			"Environment variable: ETCD_PEER_PORT")
-	flag.StringVar(&svc.etcdMajorVersion, "etcd-major-version", defaultEtcdMajorVersion,
+
+	etcdMajorVersion = flag.Int64("etcd-major-version", 3,
 		"Etcd API version (2, 3). "+
 			"Environment variable: ETCD_MAJOR_VERSION")
+
+	etcdClientPort = flag.Int64("etcd-client-port", 2379,
+		"Etcd client port number. "+
+			"Environment variable: ETCD_CLIENT_PORT")
+
+	etcdPeerPort = flag.Int64("etcd-peer-port", 2380,
+		"Etcd peer port number. "+
+			"Environment variable: ETCD_PEER_PORT")
+
+	etcdCertFile = flag.String("etcd-cert-file", os.Getenv("ETCD_CERT_FILE"),
+		"Path to the client server TLS cert file. "+
+			"Environment variable: ETCD_CERT_FILE")
+
+	etcdKeyFile = flag.String("etcd-key-file", os.Getenv("ETCD_KEY_FILE"),
+		"Path to the client server TLS key file. "+
+			"Environment variable: ETCD_KEY_FILE")
+
+	etcdClientCertAuth := flag.Bool("etcd-client-cert-auth", false,
+		"Enable client cert authentication. "+
+			"Environment variable: ETCD_CLIENT_CERT_AUTH")
+
+	etcdTrustedCaFile = flag.String("etcd-trusted-ca-file", os.Getenv("ETCD_TRUSTED_CA_FILE"),
+		"Path to the client server TLS trusted CA key file. "+
+			"Environment variable: ETCD_TRUSTED_CA_FILE")
+
+	etcdPeerCertFile := flag.String("etcd-peer-cert-file", os.Getenv("ETCD_PEER_CERT_FILE"),
+		"Path to the peer server TLS cert file. "+
+			"Environment variable: ETCD_PEER_CERT_FILE")
+
+	etcdPeerKeyFile := flag.String("etcd-peer-key-file", os.Getenv("ETCD_PEER_KEY_FILE"),
+		"Path to the peer server TLS key file. "+
+			"Environment variable: ETCD_PEER_KEY_FILE")
+
+	etcdPeerClientCertAuth := flag.Bool("etcd-peer-client-cert-auth", false,
+		"Enable peer client cert authentication. "+
+			"Environment variable: ETCD_PEER_CLIENT_CERT_AUTH")
+
+	etcdPeerTrustedCaFile := flag.String("etcd-peer-trusted-ca-file", os.Getenv("ETCD_PEER_TRUSTED_CA_FILE"),
+		"Path to the peer client server TLS trusted CA key file. "+
+			"Environment variable: ETCD_PEER_TRUSTED_CA_FILE")
+
+	etcdHeartbeatInterval = flag.Int64("etcd-heartbeat-interval", 100,
+		"Time (in milliseconds) of a heartbeat interval. "+
+			"Environment variable: ETCD_HEARTBEAT_INTERVAL")
+
+	etcdElectionTimeout = flag.Int64("etcd-election-timeout", 500,
+		"Time (in milliseconds) for an election to timeout. "+
+			"Environment variable: ETCD_ELECTION_TIMEOUT")
+
 	flag.Parse()
 
-	peerProtocol = "http"
+	clientTLSEnabled = false
 	clientProtocol = "http"
-	etcdClientPort = "2379"
-	etcdElectionTimeout = 500
-	etcdHeartbeatInterval = 100
+	if *etcdCertFile != "" {
+		clientTLSEnabled = true
+		*etcdClientCertAuth = true
+		*etcdPeerClientCertAuth = true
+		clientProtocol = "https"
+	}
+	peerProtocol = "http"
+	if *etcdPeerCertFile != "" {
+		peerProtocol = "https"
+	}
+
 	awsSession := session.New()
 	if region := os.Getenv("AWS_REGION"); region != "" {
 		awsSession.Config.WithRegion(region)
@@ -127,8 +187,8 @@ func main() {
 		var etcdClient *clientv3.Client
 		for {
 			log.Printf("etcd connecting")
-			etcdClient, err = getEtcdClient([]string{fmt.Sprintf("%s://%s:%s",
-				clientProtocol, *localInstance.PrivateIpAddress, etcdClientPort)})
+			etcdClient, err = getEtcdClient([]string{fmt.Sprintf("%s://%s:%d",
+				clientProtocol, *localInstance.PrivateIpAddress, *etcdClientPort)})
 			if err != nil {
 				log.Fatalf("ERROR: %s", err)
 			}
@@ -156,22 +216,23 @@ func main() {
 	cmd.Env = []string{
 		fmt.Sprintf("ETCD_NAME=%s", *localInstance.InstanceId),
 		fmt.Sprintf("ETCD_DATA_DIR=%s", "/var/lib/etcd"),
-		fmt.Sprintf("ETCD_ADVERTISE_CLIENT_URLS=%s://%s:%s", clientProtocol, *localInstance.PrivateIpAddress, etcdClientPort),
-		fmt.Sprintf("ETCD_LISTEN_CLIENT_URLS=%s://0.0.0.0:%s", clientProtocol, etcdClientPort),
-		fmt.Sprintf("ETCD_LISTEN_PEER_URLS=%s://0.0.0.0:%s", peerProtocol, etcdPeerPort),
+		fmt.Sprintf("ETCD_ADVERTISE_CLIENT_URLS=%s://%s:%d", clientProtocol, *localInstance.PrivateIpAddress, *etcdClientPort),
+		fmt.Sprintf("ETCD_DATA_DIR=%s", *dataDir),
+		fmt.Sprintf("ETCD_LISTEN_CLIENT_URLS=%s://0.0.0.0:%d", clientProtocol, *etcdClientPort),
+		fmt.Sprintf("ETCD_LISTEN_PEER_URLS=%s://0.0.0.0:%d", peerProtocol, *etcdPeerPort),
 		fmt.Sprintf("ETCD_INITIAL_CLUSTER_STATE=%s", initialClusterState),
 		fmt.Sprintf("ETCD_INITIAL_CLUSTER=%s", strings.Join(initialCluster, ",")),
-		fmt.Sprintf("ETCD_INITIAL_ADVERTISE_PEER_URLS=%s://%s:%s", peerProtocol, *localInstance.PrivateIpAddress, etcdPeerPort),
-		fmt.Sprintf("ETCD_HEARTBEAT_INTERVAL=%d", etcdHeartbeatInterval),
-		fmt.Sprintf("ETCD_ELECTION_TIMEOUT=%d", etcdElectionTimeout),
-		// fmt.Sprintf("ETCD_CERT_FILE=%s", *etcdCertFile),
-		// fmt.Sprintf("ETCD_KEY_FILE=%s", *etcdKeyFile),
-		// fmt.Sprintf("ETCD_CLIENT_CERT_AUTH=%s", *etcdClientCertAuth),
-		// fmt.Sprintf("ETCD_TRUSTED_CA_FILE=%s", *etcdTrustedCaFile),
-		// fmt.Sprintf("ETCD_PEER_CERT_FILE=%s", *etcdPeerCertFile),
-		// fmt.Sprintf("ETCD_PEER_KEY_FILE=%s", *etcdPeerKeyFile),
-		// fmt.Sprintf("ETCD_PEER_CLIENT_CERT_AUTH=%s", *etcdPeerClientCertAuth),
-		// fmt.Sprintf("ETCD_PEER_TRUSTED_CA_FILE=%s", *etcdPeerTrustedCaFile),
+		fmt.Sprintf("ETCD_INITIAL_ADVERTISE_PEER_URLS=%s://%s:%d", peerProtocol, *localInstance.PrivateIpAddress, *etcdPeerPort),
+		fmt.Sprintf("ETCD_HEARTBEAT_INTERVAL=%d", *etcdHeartbeatInterval),
+		fmt.Sprintf("ETCD_ELECTION_TIMEOUT=%d", *etcdElectionTimeout),
+		fmt.Sprintf("ETCD_CERT_FILE=%s", *etcdCertFile),
+		fmt.Sprintf("ETCD_KEY_FILE=%s", *etcdKeyFile),
+		fmt.Sprintf("ETCD_CLIENT_CERT_AUTH=%v", *etcdClientCertAuth),
+		fmt.Sprintf("ETCD_TRUSTED_CA_FILE=%s", *etcdTrustedCaFile),
+		fmt.Sprintf("ETCD_PEER_CERT_FILE=%s", *etcdPeerCertFile),
+		fmt.Sprintf("ETCD_PEER_KEY_FILE=%s", *etcdPeerKeyFile),
+		fmt.Sprintf("ETCD_PEER_CLIENT_CERT_AUTH=%v", *etcdPeerClientCertAuth),
+		fmt.Sprintf("ETCD_PEER_TRUSTED_CA_FILE=%s", *etcdPeerTrustedCaFile),
 	}
 	asg, _ := s.AutoscalingGroup()
 	if asg != nil {
@@ -203,8 +264,8 @@ func buildCluster(s *awsutils.Cluster) (initialClusterState string, initialClust
 		log.Printf("getting stats from %s (%s)", *instance.InstanceId, *instance.PrivateIpAddress)
 
 		// add this instance to the initialCluster expression
-		initialCluster = append(initialCluster, fmt.Sprintf("%s=%s://%s:%s",
-			*instance.InstanceId, peerProtocol, *instance.PrivateIpAddress, etcdPeerPort))
+		initialCluster = append(initialCluster, fmt.Sprintf("%s=%s://%s:%d",
+			*instance.InstanceId, peerProtocol, *instance.PrivateIpAddress, *etcdPeerPort))
 
 		// skip the local node, since we know it is not running yet
 		if *instance.InstanceId == *localInstance.InstanceId {
@@ -213,23 +274,23 @@ func buildCluster(s *awsutils.Cluster) (initialClusterState string, initialClust
 		path := "stats/self"
 		resp, err := getAPIResponse(*instance.PrivateIpAddress, *instance.InstanceId, path, http.MethodGet)
 		if err != nil {
-			log.Printf("%s: %s://%s:%s/v2/%s: %s", *instance.InstanceId, clientProtocol,
-				*instance.PrivateIpAddress, etcdClientPort, path, err)
+			log.Printf("%s: %s://%s:%d/v2/%s: %s", *instance.InstanceId, clientProtocol,
+				*instance.PrivateIpAddress, *etcdClientPort, path, err)
 			continue
 		}
 		nodeState := etcdState{}
 		if err := json.NewDecoder(resp.Body).Decode(&nodeState); err != nil {
-			log.Printf("%s: %s://%s:%s/v2/%s: %s", *instance.InstanceId, clientProtocol,
-				*instance.PrivateIpAddress, etcdClientPort, path, err)
+			log.Printf("%s: %s://%s:%d/v2/%s: %s", *instance.InstanceId, clientProtocol,
+				*instance.PrivateIpAddress, *etcdClientPort, path, err)
 			continue
 		}
 		if nodeState.LeaderInfo.Leader == "" {
-			log.Printf("%s: %s://%s:%s/v2/%s: alive, no leader", *instance.InstanceId, clientProtocol,
-				*instance.PrivateIpAddress, etcdClientPort, path)
+			log.Printf("%s: %s://%s:%d/v2/%s: alive, no leader", *instance.InstanceId, clientProtocol,
+				*instance.PrivateIpAddress, *etcdClientPort, path)
 			continue
 		}
-		log.Printf("%s: %s://%s:%s/v2/%s: has leader %s", *instance.InstanceId, clientProtocol,
-			*instance.PrivateIpAddress, etcdClientPort, path, nodeState.LeaderInfo.Leader)
+		log.Printf("%s: %s://%s:%d/v2/%s: has leader %s", *instance.InstanceId, clientProtocol,
+			*instance.PrivateIpAddress, *etcdClientPort, path, nodeState.LeaderInfo.Leader)
 		if initialClusterState != "existing" {
 			initialClusterState = "existing"
 			// inform the node we found about the new node we're about to add so that
@@ -237,8 +298,8 @@ func buildCluster(s *awsutils.Cluster) (initialClusterState string, initialClust
 			log.Printf("joining cluster via %s", *instance.InstanceId)
 			m := etcdMember{
 				Name: *localInstance.InstanceId,
-				PeerURLs: []string{fmt.Sprintf("%s://%s:%s",
-					peerProtocol, *localInstance.PrivateIpAddress, etcdPeerPort)},
+				PeerURLs: []string{fmt.Sprintf("%s://%s:%d",
+					peerProtocol, *localInstance.PrivateIpAddress, *etcdPeerPort)},
 			}
 			body, _ := json.Marshal(m)
 			getAPIResponseWithBody(*instance.PrivateIpAddress, *instance.InstanceId, "members", http.MethodPost, "application/json", bytes.NewReader(body))
@@ -247,51 +308,101 @@ func buildCluster(s *awsutils.Cluster) (initialClusterState string, initialClust
 	return initialClusterState, initialCluster, nil
 }
 
-func getAPIResponse(privateIpAddress string, instanceId string, path string, method string) (*http.Response, error) {
-	return getAPIResponseWithBody(privateIpAddress, instanceId, path, method, "", nil)
+func getAPIResponse(privateIPAddress string, instanceID string, path string, method string) (*http.Response, error) {
+	return getAPIResponseWithBody(privateIPAddress, instanceID, path, method, "", nil)
 }
 
-func getAPIResponseWithBody(privateIpAddress string, instanceId string, path string, method string, bodyType string, body io.Reader) (*http.Response, error) {
+func getAPIResponseWithBody(privateIPAddress string, instanceID string, path string, method string, bodyType string, body io.Reader) (*http.Response, error) {
 	var resp *http.Response
 	var err error
+	var apiVersion string
 	var req *http.Request
-	if bodyType == "" {
-		req, _ = http.NewRequest(method, fmt.Sprintf("%s://%s:%s/v2/%s",
-			clientProtocol, privateIpAddress, etcdClientPort, path), body)
+	if path != "health" {
+		apiVersion = "/v2"
+	}
+	req, err = http.NewRequest(method, fmt.Sprintf("%s://%s:%d%s/%s",
+		clientProtocol, privateIPAddress, *etcdClientPort, apiVersion, path), body)
+
+	if err != nil {
+		return resp, fmt.Errorf("%s: %s %s: %s",
+			instanceID, method, req.URL, err)
+	}
+	if method != "GET" && method != "DELETE" && bodyType != "" {
+		req.Header.Add("Content-Type", bodyType)
 	}
 	client, err := getHTTPClient()
-	if bodyType != "" {
-		client.Post(fmt.Sprintf("%s://%s:%s/v2/%s",
-			clientProtocol, privateIpAddress, etcdClientPort, path), bodyType, body)
-	} else {
-		resp, err = client.Do(req)
-	}
 	if err != nil {
-		return nil, fmt.Errorf("%s: %s %s://%s:%s/v2/%s: %s",
-			instanceId, method, clientProtocol, privateIpAddress, etcdClientPort, path, err)
+		return resp, fmt.Errorf("%s: %s %s: %s",
+			instanceID, method, req.URL, err)
+	}
+	resp, err = client.Do(req)
+
+	if err != nil {
+		return resp, fmt.Errorf("%s: %s %s: %s",
+			instanceID, method, req.URL, err)
 	}
 	return resp, nil
 }
 
 func getHTTPClient() (*http.Client, error) {
 	var transport *http.Transport
-	transport = &http.Transport{}
-	client := &http.Client{
-		Timeout:   time.Duration(5 * time.Second),
-		Transport: transport,
+	if clientTLSEnabled {
+		cert, err := tls.LoadX509KeyPair(*etcdCertFile, *etcdKeyFile)
+		if err != nil {
+			return nil, fmt.Errorf("ERROR: %s", err)
+		}
+		caCert, err := ioutil.ReadFile(*etcdTrustedCaFile)
+		if err != nil {
+			return nil, fmt.Errorf("ERROR: %s", err)
+		}
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCertPool,
+		}
+		tlsConfig.BuildNameToCertificate()
+		transport = &http.Transport{
+			TLSClientConfig:     tlsConfig,
+			TLSHandshakeTimeout: 2 * time.Second,
+		}
+	} else {
+		transport = &http.Transport{}
 	}
+	transport.IdleConnTimeout = 10 * time.Second
+	transport.ResponseHeaderTimeout = 2 * time.Second
+	transport.ExpectContinueTimeout = 10 * time.Second
+	client := &http.Client{Transport: transport, Timeout: 20 * time.Second}
 	return client, nil
 }
 
 func getEtcdClient(endpoints []string) (*clientv3.Client, error) {
 	var etcdClient *clientv3.Client
 	var err error
-
-	etcdClient, err = clientv3.New(clientv3.Config{
-		Endpoints: endpoints,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("ERROR: %s", err)
+	if clientTLSEnabled {
+		tlsInfo := transport.TLSInfo{
+			CertFile:      *etcdCertFile,
+			KeyFile:       *etcdKeyFile,
+			TrustedCAFile: *etcdTrustedCaFile,
+		}
+		tlsConfig, err := tlsInfo.ClientConfig()
+		if err != nil {
+			return nil, fmt.Errorf("ERROR: %s", err)
+		}
+		etcdClient, err = clientv3.New(clientv3.Config{
+			Endpoints: endpoints,
+			TLS:       tlsConfig,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("ERROR: %s", err)
+		}
+	} else {
+		etcdClient, err = clientv3.New(clientv3.Config{
+			Endpoints: endpoints,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("ERROR: %s", err)
+		}
 	}
 	return etcdClient, nil
 }
