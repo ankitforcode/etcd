2421e8237f00c9bd3e150cb9bbf587f18ac31bf7
ankitforcode <its_ankit1@yahoo.co.in>
2018-01-17 11:38:59 +0530

    fixing the endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
@@ -21,6 +22,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/coreos/etcd/clientv3"
 	"github.com/coreos/etcd/pkg/transport"
+	"google.golang.org/grpc/grpclog"
 )
 
 // Service :
@@ -178,31 +180,33 @@ func main() {
 	}
 	initialClusterState, initialCluster, err := buildCluster(s)
 	log.Printf("initial cluster: %s %s", initialClusterState, initialCluster)
-	// go func() {
-	// 	// wait for etcd to start
-	// 	var etcdClient *clientv3.Client
-	// 	for {
-	// 		log.Printf("etcd connecting")
-	// 		etcdClient, err = getEtcdClient([]string{fmt.Sprintf("%s://%s:%d",
-	// 			clientProtocol, *localInstance.PrivateDnsName, etcdClientPort)})
-	// 		if err != nil {
-	// 			log.Fatalf("ERROR: %s", err)
-	// 		}
-	// 		defer etcdClient.Close()
-	// 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	// 		err := etcdClient.Sync(ctx)
-	// 		cancel()
-	// 		if err != nil {
-	// 			log.Printf("waiting for etcd to start: %s", err)
-	// 		} else {
-	// 			log.Printf("etcd connected")
-	// 			resp, _ := etcdClient.MemberList(context.Background())
-	// 			log.Printf("etcd members: %s", resp.Members)
-	// 			break
-	// 		}
-	// 		time.Sleep(time.Second)
-	// 	}
-	// }()
+	go func() {
+		// wait for etcd to start
+		var etcdClient *clientv3.Client
+		clientv3.SetLogger(grpclog.NewLoggerV2(os.Stderr, os.Stderr, os.Stderr))
+
+		for {
+			log.Printf("etcd connecting")
+			etcdClient, err = getEtcdClient([]string{fmt.Sprintf("%s://%s:%d",
+				clientProtocol, *localInstance.PrivateIpAddress, etcdClientPort)})
+			if err != nil {
+				log.Fatalf("ERROR: %s", err)
+			}
+			defer etcdClient.Close()
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			err := etcdClient.Sync(ctx)
+			cancel()
+			if err != nil {
+				log.Printf("waiting for etcd to start: %s", err)
+			} else {
+				log.Printf("etcd connected")
+				resp, _ := etcdClient.MemberList(context.Background())
+				log.Printf("etcd members: %s", resp.Members)
+				break
+			}
+			time.Sleep(time.Second)
+		}
+	}()
 
 	go watchLifecycleEvents(s, *lifecycleQueueName)
 
@@ -254,7 +258,7 @@ func buildCluster(s *awsutils.Cluster) (initialClusterState string, initialClust
 	initialClusterState = "new"
 	initialCluster = []string{}
 	for _, instance := range clusterInstances {
-		if instance.PrivateDnsName == nil {
+		if *instance.PrivateDnsName == "" {
 			continue
 		}
 		log.Printf("getting stats from %s (%s)", *instance.InstanceId, *instance.PrivateDnsName)
